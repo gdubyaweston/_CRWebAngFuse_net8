5dfa43eece026d925942dd8dff620809d1b4d571
gweston@aictitle.com <gweston@aictitle.com>
2024-07-16 13:10:25 -0500

    testing urls

--- src/app/modules/_environment/environment.ts
+++ src/app/modules/_environment/environment.ts
@@ -1,9 +1,24 @@
 export const environment = {
     production: true,
-    urlAddress: 'https://localhost:7001'
+    urlAddress: 'https://dev-api.aircraftclosingroom.com'
 };
 
 /*
+
+export const environment = {
+    production: true,
+    urlAddress: 'https://dev-cl.aircraftclosingroom.com'
+};
+
+export const environment = {
+    production: true,
+    urlAddress: 'https://dev-api.aircraftclosingroom.com'
+};
+
+export const environment = {
+    production: true,
+    urlAddress: 'https://localhost:7001'
+};
 
 export const environment = {
     production: true,
